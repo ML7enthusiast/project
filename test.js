efd9fd547460801e91e8ba88fd1e99fa1c809ee4
ML7enthusiast <vaishnavihengane@gmail.com>
2026-07-02 15:29:05 +0530

    first

---
+++ test.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const http = require('http');
+
+const app = express();
+app.get('/', (req,res) => res.send('hello from express app'))
+
+const server = app.listen(5000, () => {
+    console.log('server started, running smoke test...');
+    http.get('http://localhost:5000/', (res) =>{
+        console.log('status: ${res.statusCode}');
+        if (res.statusCode === 200) {
+            console.log('smaoke test passed');
+            server.close();
+            process.exit(0);
+        } else {
+            server.close();
+            process.exit(1);
+
+        }
+        
+    }).on('error', (err) => {
+        console.error('request failed:', err.message);
+        server.close();
+        process.exit(1);
+    });
+});
